ae45ebeea5c82d1ba29341c5f9b339a44505bdb5
clover_htc <clover@haituncun.com>
2017-05-23 17:36:24 +0800

    Initial Commit

---
+++ FYHttpDNSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYHttpDNSDemo
+//
+//  Created by Clover on 5/23/17.
+//  Copyright © 2017 Clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYHttpDNSDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FYHttpDNSDemo
+//
+//  Created by Clover on 5/23/17.
+//  Copyright © 2017 Clover. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
